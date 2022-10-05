d2671f1d774e01f0f5ff074e34363b56388597c4
Ahmer Raza <ahhmerrr.dev@gmail.com>
2022-10-05 14:49:14 -0400

    Begin portability 💼

--- scripts/etc.js
+++ scripts/etc.js
@@ -6,6 +6,7 @@ const pallette =
 const fpsElement = document.getElementById("fps");
 const screenElement = document.getElementById("screen");
 const canvasElement = document.getElementById("canvas");
+const videoElement = document.getElementById("video");
 const nocamElement = document.getElementById("nocam");
 
 const settingsBtnElement = document.getElementById("settingsBtn");
@@ -16,9 +17,10 @@ const divisor = 1;
 
 let contrast = 128;
 let brightness = 0;
-
 let fps = 30;
+
 let playing = false;
+let running = true;
 
 Number.prototype.clamp = function (min, max) {
     return Math.min(Math.max(this, min), max);
--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,24 @@
-navigator.mediaDevices
-    .getUserMedia({
-        audio: false,
-        video: true,
-    })
-    .then((stream) => camera(stream))
-    .catch((err) => console.log({ err }));
+// use ImageCapture API
+if ("ImageCapture" in window) {
+    navigator.mediaDevices
+        .getUserMedia({
+            audio: false,
+            video: true,
+        })
+        .then((stream) => camera(stream))
+        .catch((err) => console.log({ err }));
+}
+// use built-in HTML5 video API
+else {
+}
 
-function camera(stream) {
+const camera = (stream) => {
+    // hide/show extra stuff
     fpsElement.style.display = "block";
     nocamElement.style.display = "none";
     playing = true;
 
+    // create an ImageCapture object for grabbing frames from the camera
     let capture = new ImageCapture(stream.getVideoTracks()[0]);
 
     let time = 0;
@@ -31,5 +39,12 @@ function camera(stream) {
                 "fps: " + Math.round((1 / (performance.now() - time)) * 1000);
             time = performance.now();
         }
+
+        if (!running) {
+            stream.getVideoTracks()[0].stop();
+            capture.closoe;
+        }
     }, 1000 / fps);
-}
+};
+
+const otherCamera = (stream) => {};
